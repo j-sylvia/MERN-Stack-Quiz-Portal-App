9fb02db8cc0c13970c5353c920dc257d7cd42d2a
j-sylvia <150928883+j-sylvia@users.noreply.github.com>
2024-08-29 15:56:41 +0530

    Update authcontroller.js

--- quizbackend/controllers/authcontroller.js
+++ quizbackend/controllers/authcontroller.js
@@ -1,8 +1,10 @@
 const User = require("../models/users");
 const Course = require("../models/courses")
 const Quiz=require("../models/questions")
+const Exam = require('../models/exam');
 const {createSecretToken}=require('../util/secrettoken')
 const bcrypt = require("bcrypt");
+const { Types } = require('mongoose');
 
 module.exports.Signup=async (req,res,next)=>{
     try{
@@ -23,7 +25,7 @@ module.exports.Signup=async (req,res,next)=>{
     // });
     res
         .status(201)
-        .json({message: "User signed in successfully", success:true, user});
+        .json({message: "User created successfully", success:true, user});
         next();
     }
 } catch(error){
@@ -38,7 +40,7 @@ module.exports.Login=async (req,res,next)=>{
             return res.json({message:"Please provide email and password"});
         }
         const user = await User.findOne({email});
-        console.log(user)
+        console.log("userId:",user._id)
         if(!user){
             return res.json({message:"User does not exist"});
         }
@@ -48,21 +50,30 @@ module.exports.Login=async (req,res,next)=>{
         }
         if (user.isAdmin) {
             const token = createSecretToken(user._id);
+            console.log("Token:",token)
+            console.log("Token Cookie",req.cookies);
+            // res.setHeader("Authorization",`Bearer ${token}`);
             res.cookie("token", token, {
                 withCredentials: true,
                 httpOnly: false,
             });
+            res.cookie("userId", user.id, { withCredentials: true });
+            res.cookie("userName", user.name, { withCredentials: true });
+            res.cookie("role", user.isAdmin, { withCredentials: true });
             console.log("Auth controller",user.isAdmin)
-            return res.status(201).json({message: "User logged in successfully", success: true, user,token});
+            return res.status(201).json({message: "User logged in successfully", success: true, user,token,id:user._id,name:user.name,role:user.isAdmin});
             // return res.redirect('/users/create-course');
         } else {
             const token = createSecretToken(user._id);
+            console.log("Token:",token)
+            console.log("Token Cookie",req.cookies);
             res.cookie("token", token, {
                 withCredentials: true,
                 httpOnly: false,
             });
             console.log("Auth controller",user.isAdmin)
-            return res.status(201).json({message: "User logged in successfully", success: true, user,token});
+            console.log("Auth controller",user.email)
+            return res.status(201).json({message: "User logged in successfully", success: true, user,token,id:user._id,name:user.name,role:user.isAdmin});
             // return res.redirect('/users/attend-exam');
         }   
     } catch(error){
@@ -71,63 +82,3 @@ module.exports.Login=async (req,res,next)=>{
     }
 }
 
-module.exports.Course=async (req,res,next)=>{
-    try{
-        const {name,description,createdAt}=req.body;
-        const existingCourse=await Course.findOne({name});
-        if(!name || !description){
-            return res.json({message:"Please provide name and description"});
-        }
-        if(existingCourse){
-            return res.json({message:"Course already exists"});
-        }
-        else{
-        const course=await Course.create({name,description,createdAt});
-    //     const token=createSecretToken(course._id);
-    //     res.cookie("token", token, {
-    //         withCredentials: true,
-    //         httpOnly:false,
-    // });
-    res
-        .status(201)
-        .json({message: "Course created successfully", success:true, course});
-        next();
-    }
-} catch(error){
-    console.error(error);
-}
-};
-
-module.exports.Quiz = async (req, res, next) => {
-    try {
-      const { course, text, options } = req.body;
-  
-      // Check if the user provided necessary information
-      if (!course || !text || !options) {
-        return res.status(400).json({ message: "Please provide course, text, and options for the question." });
-      }
-  
-      const coursename = await Course.findOne({ name: course });
-
-    if (!coursename) {
-      return res.status(400).json({ error: `Course with name ${course} not found` });
-    }
-
-      // Create a new question document
-      const newQuestion = await Quiz.create({
-        course,
-        text,
-        options
-      });
-  
-      return res.status(201).json({
-        message: "Question created successfully",
-        success: true,
-        question: newQuestion
-      });
-  
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: "An error occurred while creating the question." });
-    }
-  };
