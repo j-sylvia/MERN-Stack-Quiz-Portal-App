8d44150fcc3887e3ebe796d72d4696c7f1fd08fe
j-sylvia <150928883+j-sylvia@users.noreply.github.com>
2024-08-29 15:57:50 +0530

    Update authmiddleware.js

--- quizbackend/middlewares/authmiddleware.js
+++ quizbackend/middlewares/authmiddleware.js
@@ -1,25 +1,37 @@
-const User = require("../models/users");
 const jwt = require("jsonwebtoken");
+const User = require("../models/users");
 require("dotenv").config();
 
-module.exports.userVerification=(req,res,next)=>{
-    const token=req.cookies.token
-    if (!token){
-        return res.json({status:false})
+module.exports.userVerification = async (req, res, next) => {
+  const token = req.cookies.token
+  console.log("Middleware Token Cookie:",req.cookies);
+  console.log("Token:",token)
+  if (!token) {
+    return res.json({ status: false });
+  }
+
+  try {
+    const data = jwt.verify(token, process.env.TOKEN_KEY);
+    console.log("Decoded:",data)
+    req.user=await User.findById(data.id).select('-password');
+    const user = await User.findById(data.id);
+    if (user) {
+      console.log("Auth Middleware user.isAdmin", user.isAdmin);
+      console.log(user)
+      // Set the cookie 
+      res.cookie("token", token, {
+        withCredentials: true,
+        httpOnly: false,
+      });
+      res.cookie("userId", user.id, {httpOnly: true, withCredentials: true });
+      res.cookie("userName", user.name, {httpOnly: true, withCredentials: true });
+      res.cookie("role", user.isAdmin, {httpOnly: true, withCredentials: true });
+      console.log("Auth controller", user.isAdmin);
+      return res.json({ status: true, data:req.user,user:user.name, isAdmin: user.isAdmin,role:user.isAdmin });
+    } else {
+      return res.json({ status: false,message:"User not found" });
     }
-    jwt.verify(token,process.env.TOKEN_KEY,async (err,data)=>{
-        if (err){
-            return res.json({status:false})
-        }
-        else{
-            const user = await User.findById(data.id);
-            if (user){
-                console.log("Auth Middleware",user.isAdmin)
-                return res.json({status:true,user:user.name,isAdmin:user.isAdmin})
-            }
-            else{
-                return res.json({status:false})
-            }
-        }
-    })
-}
+  } catch (err) {
+    return res.json({ status: false, message:"authmiddleware catch" });
+  }
+};
